5c6313e7041a0cbf5354c22f1878648322e54218
20537145 <141738221+20537145@users.noreply.github.com>
2023-10-28 01:36:07 +0100

    finalynro

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import {
 import Movies from "./components/Movies";
 import Navlayout from "./components/Navlayout";
 import Trailer from "./components/Trailer";
+import Home from "./Home";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Navlayout />}>
+      <Route index element={<Home/>}/>
       <Route path="movies" element={<Movies />} />
       <Route path="trailer" element={<Trailer />} />
     </Route>
